c1ad592d41e06acad4995115280b245b11e17951
Tashu prajapati <tashu.ug22@nsut.ac.in>
2025-07-03 23:40:12 +0530

    Update Modal.js

--- client/src/components/Common/Modal.js
+++ client/src/components/Common/Modal.js
@@ -7,7 +7,7 @@ export const Modal = ({ isOpen, onClose, title, children }) => {
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
       <div className="bg-purple-400 focus:ring-purple-500 rounded-lg p-6 max-w-md w-full">
         <div className="flex justify-between items-center mb-4">
-          <h2 className="text-xl font-bold text-gray-50">{title}</h2>
+          <h2 className="text-xl font-bold text-gray-50">{title}</h2> 
           <button
             onClick={onClose}
             className="text-white hover:text-gray-150"
