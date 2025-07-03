697e54737021bea68966dea48be7da62e8f95e82
Tashu prajapati <tashu.ug22@nsut.ac.in>
2025-07-03 23:37:21 +0530

    Update RoomManagement.js

--- client/src/components/Admin/RoomManagement.js
+++ client/src/components/Admin/RoomManagement.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import { useAuth } from '../../context/AuthContext';
 import { Modal } from '../Common/Modal';
-
+ 
 const RoomManagement = () => {
   const { user } = useAuth();
   const [rooms, setRooms] = useState([]);
