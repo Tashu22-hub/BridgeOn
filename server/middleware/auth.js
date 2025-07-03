8e09f0c85687a5d7729c1a816ec0a24540dbe40a
Tashu prajapati <tashu.ug22@nsut.ac.in>
2025-07-03 23:31:58 +0530

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const authenticateToken = (req, res, next) => {
     
     const token = req.headers['authorization']?.split(' ')[1];
-    if (!token) return res.status(401).json({ error: 'No token provided' });
+    if (!token) return res.status(401).json({ error: 'No token provided' }); 
 
     
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
