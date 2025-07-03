82f8b14439f2a9d7660f2b899c99b1d2856d2651
Tashu prajapati <tashu.ug22@nsut.ac.in>
2025-07-03 23:39:33 +0530

    Update RoomList.js

--- client/src/components/Chat/RoomList.js
+++ client/src/components/Chat/RoomList.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import { Modal } from '../Common/Modal';
-
+ 
 export const RoomList = () => {
   const { user } = useAuth();
   const [rooms, setRooms] = useState([]);
